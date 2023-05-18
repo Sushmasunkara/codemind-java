2185307380b23660a176162eed1a833376cdf3cf
Sushmasunkara <119720369+Sushmasunkara@users.noreply.github.com>
2023-05-18 01:02:43 -0400

    import java.util.*; public class ColorCode {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch;         ch=sc.nextLine().charAt(0);         if(ch=='v'||ch=='v')         System.out.printf("Violet");         else if(ch=='i'||ch=='I')         System.out.printf("Indigo");         else if(ch=='b'||ch=='B')         System.out.printf("Blue");          else if(ch=='g'||ch=='G')         System.out.printf("Green");          else if(ch=='y'||ch=='Y')         System.out.printf("Yellow");          else if(ch=='o'||ch=='O')            System.out.printf("Orange");             else if(ch=='r'||ch=='R')              System.out.printf("Red");               else               System.out.printf("-1");     } }

---
+++ Color_Code.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ColorCode
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        ch=sc.nextLine().charAt(0);
+        if(ch=='v'||ch=='v')
+        System.out.printf("Violet");
+        else if(ch=='i'||ch=='I')
+        System.out.printf("Indigo");
+        else if(ch=='b'||ch=='B')
+        System.out.printf("Blue");
+         else if(ch=='g'||ch=='G')
+        System.out.printf("Green");
+         else if(ch=='y'||ch=='Y')
+        System.out.printf("Yellow");
+         else if(ch=='o'||ch=='O')
+           System.out.printf("Orange");
+            else if(ch=='r'||ch=='R')
+             System.out.printf("Red");
+              else 
+              System.out.printf("-1");
+    }
+}
